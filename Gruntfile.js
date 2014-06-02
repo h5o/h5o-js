1d43a677966bb66c51cefad091b85ba1c5362a57
Dominykas Blyžė <hello@dominykas.com>
2014-06-03 00:35:57 +0300

    an attempt at npm prepublish script

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,11 +6,15 @@ module.exports = function (grunt) {
 	var VERSION = require("./package.json").version;
 
 	grunt.initConfig({
-		"clean": [ "dist/**" ],
+		"clean": {
+			"all": [ "dist/**" ],
+			"debug": [ "dist/debug/**" ],
+			"min": [ "dist/min/**" ]
+		},
 		"copy": {
 			"bookmarklet-js": {
 				"src": [ "src/HTML5OutlineBookmarklet.js" ],
-				"dest": "dist/HTML5OutlineBookmarklet.debug.js"
+				"dest": "dist/debug/HTML5OutlineBookmarklet.debug.js"
 			}
 		},
 		"concat": {
@@ -27,30 +31,30 @@ module.exports = function (grunt) {
 					"src/HTML5Outline.js",
 					"src/_foot.js"
 				],
-				"dest": "dist/outliner.debug.js"
+				"dest": "dist/debug/outliner.debug.js"
 			},
 			"outliner-release": {
 				"src": [
 					"src/notice.txt",
-					"dist/outliner.min.js"
+					"dist/min/outliner.min.js"
 				],
 				"dest": "dist/outliner." + VERSION + ".js"
 			}
 		},
 		"uglify": {
 			"bookmarklet-js": {
-				"src": [ "dist/HTML5OutlineBookmarklet.debug.js" ],
-				"dest": "dist/HTML5OutlineBookmarklet.min.js"
+				"src": [ "dist/debug/HTML5OutlineBookmarklet.debug.js" ],
+				"dest": "dist/min/HTML5OutlineBookmarklet.min.js"
 			},
 			"outliner-js": {
-				"src": [ "dist/outliner.debug.js" ],
-				"dest": "dist/outliner.min.js"
+				"src": [ "dist/debug/outliner.debug.js" ],
+				"dest": "dist/min/outliner.min.js"
 			}
 		}
 	});
 
-	grunt.registerTask("default", "Clean build and minify", [ "clean", "concat:outliner-js", "copy:bookmarklet-js", "uglify" ]);
-	grunt.registerTask("release", "Prepare files for deployment", [ "default", "concat:outliner-release", "_bookmarklet-release" ]);
+	grunt.registerTask("default", "Clean build and minify", [ "clean:all", "concat:outliner-js", "copy:bookmarklet-js", "uglify" ]);
+	grunt.registerTask("release", "Prepare files for deployment", [ "default", "concat:outliner-release", "_bookmarklet-release", "clean:debug", "clean:min" ]);
 
 	grunt.registerTask("_bookmarklet-release", "Prepare bookmarklet HTML for release", function () {
 		var done = this.async();
@@ -60,8 +64,8 @@ module.exports = function (grunt) {
 
 		bookmarkletHtml = bookmarkletHtml.replace(/@VERSION/g, VERSION);
 		bookmarkletHtml = bookmarkletHtml.replace(/@NOTICE/g, fs.readFileSync("src/notice.txt"));
-		bookmarkletHtml = bookmarkletHtml.replace(/@RUNBOOKMARKLET/g, encodeURIComponent(fs.readFileSync("dist/HTML5OutlineBookmarklet.min.js").toString()));
-		bookmarkletHtml = bookmarkletHtml.replace(/@MINIFIED/g, encodeURIComponent(fs.readFileSync("dist/outliner.min.js").toString()));
+		bookmarkletHtml = bookmarkletHtml.replace(/@RUNBOOKMARKLET/g, encodeURIComponent(fs.readFileSync("dist/min/HTML5OutlineBookmarklet.min.js").toString()));
+		bookmarkletHtml = bookmarkletHtml.replace(/@MINIFIED/g, encodeURIComponent(fs.readFileSync("dist/min/outliner.min.js").toString()));
 
 		fs.writeFile("dist/outliner." + VERSION + ".html", bookmarkletHtml, done);
 	});
