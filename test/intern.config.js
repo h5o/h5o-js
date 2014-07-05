05da589b4af782bf3c2241226e195c6f91f20cbc
Dominykas Blyžė <hello@dominykas.com>
2014-07-06 00:16:39 +0300

    ok, IE8 was probably a bad idea, considering the stuff doesn't even work yet

--- test/intern.config.js
+++ test/intern.config.js
@@ -12,7 +12,7 @@ define({
 		{ browserName: 'internet explorer', version: '11', platform: 'Windows 8.1' },
 		{ browserName: 'internet explorer', version: '10', platform: 'Windows 8' },
 		{ browserName: 'internet explorer', version: '9', platform: 'Windows 7' },
-		{ browserName: 'internet explorer', version: '8', platform: 'Windows 7' },
+//		{ browserName: 'internet explorer', version: '8', platform: 'Windows 7' },
 		{ browserName: 'firefox', platform: [ 'OS X 10.6', 'Windows 7', 'Linux' ] },
 		{ browserName: 'chrome', platform: [ 'OS X 10.6', 'Windows 7', 'Linux' ] },
 		{ browserName: 'safari', version: '6', platform: 'OS X 10.8' },
